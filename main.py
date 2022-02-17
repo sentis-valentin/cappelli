33ef55f99bc4779096953efdae13fb1bd7d74aa7
sentis Valentin <50593470+sentis-valentin@users.noreply.github.com>
2022-02-17 21:02:21 +0000

    Add files via upload
    script python de l'application linux

---
+++ main.py
@@ -0,0 +1,60 @@
+import sys
+
+from PyQt5.QtWidgets import *
+
+from PyQt5.QtWebEngineWidgets import *
+
+from PyQt5.QtCore import *
+
+class Window(QMainWindow):
+
+   
+    def __init__(self):
+       
+        super(Window,self).__init__()
+
+        
+        self.browser = QWebEngineView()
+
+        
+        self.browser.setUrl(QUrl('https://sentis-valentin.github.io/cappelli/'))
+
+        
+        self.setCentralWidget(self.browser)
+
+        
+        self.showMaximized()
+
+        
+        self.browser.urlChanged.connect(self.updateUrl)
+
+    
+    def home(self):
+        self.browser.setUrl(QUrl('https://web.archive.org/web/20160317233629/http://www.hist.msu.ru/Departments/Medieval/Cappelli/'))
+
+   
+    def loadUrl(self):
+        
+        url = self.searchBar.text()
+        
+        self.browser.setUrl(QUrl(url))
+
+    
+    def updateUrl(self, url):
+        
+        self.searchBar.setText(url.toString())
+
+
+MyApp = QApplication(sys.argv)
+
+
+QApplication.setApplicationName('Cappelli')
+
+
+window = Window()
+
+
+MyApp.exec_()
+
+# pip install PyQtWebEngine
+# pip install PyQt5
